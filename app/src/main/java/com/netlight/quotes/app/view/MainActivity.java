dcc713a849f2d45cd794508db189deb62e315d68
Jens Moser <Jens.Moser@nrk.no>
2015-09-13 16:41:23 +0200

    Removed unused imports

--- app/src/main/java/com/netlight/quotes/app/view/MainActivity.java
+++ app/src/main/java/com/netlight/quotes/app/view/MainActivity.java
@@ -1,6 +1,5 @@
 package com.netlight.quotes.app.view;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +8,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.netlight.quotes.app.R;
